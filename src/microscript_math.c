1d10cd88ddfaf3fbfe75283156bd5cb0ea40746c
magayaga <cjmagayaga957@gmail.com>
2025-05-20 09:26:10 +0800

    MicroScript's source code: Update and Fix C source file

--- src/microscript_math.c
+++ src/microscript_math.c
@@ -50,8 +50,7 @@ JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_cbrt(JNIEnv *
 }
 
 // Cube
-JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_cube
-  (JNIEnv *env, jclass cls, jdouble value) {
+JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_cube(JNIEnv *env, jclass cls, jdouble value) {
     return value * value * value;
 }
 
@@ -73,4 +72,113 @@ JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_log(JNIEnv *e
 // Logarithm base 2
 JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_log2(JNIEnv *env, jclass cls, jdouble value) {
     return log2(value);
+}
+
+// Sine
+JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_sin(JNIEnv *env, jclass cls, jdouble value) {
+    return sin(value);
+}
+
+// Cosine
+JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_cos(JNIEnv *env, jclass cls, jdouble value) {
+    return cos(value);
+}
+
+// Tangent
+JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_tan(JNIEnv *env, jclass cls, jdouble value) {
+    return tan(value);
+}
+
+// Inverse sine
+JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_asin(JNIEnv *env, jclass cls, jdouble value) {
+    return asin(value);
+}
+
+// Inverse cosine
+JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_acos(JNIEnv *env, jclass cls, jdouble value) {
+    return acos(value);
+}
+
+// Inverse tangent
+JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_atan(JNIEnv *env, jclass cls, jdouble value) {
+    return atan(value);
+}
+
+// Inverse tangent with two arguments
+JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_atan2(JNIEnv *env, jclass cls, jdouble y, jdouble x) {
+    return atan2(y, x);
+}
+
+// Hyperbolic sine
+JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_sinh(JNIEnv *env, jclass cls, jdouble value) {
+    return sinh(value);
+}
+
+// Hyperbolic cosine
+JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_cosh(JNIEnv *env, jclass cls, jdouble value) {
+    return cosh(value);
+}
+
+// Hyperbolic tangent
+JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_tanh(JNIEnv *env, jclass cls, jdouble value) {
+    return tanh(value);
+}
+
+// Hyperbolic inverse sine
+JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_asinh(JNIEnv *env, jclass cls, jdouble value) {
+    return asinh(value);
+}
+
+// Hyperbolic inverse cosine
+JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_acosh(JNIEnv *env, jclass cls, jdouble value) {
+    return acosh(value);
+}
+
+// Hyperbolic inverse tangent
+JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_atanh(JNIEnv *env, jclass cls, jdouble value) {
+    return atanh(value);
+}
+
+// Exponential function
+JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_exp(JNIEnv *env, jclass cls, jdouble value) {
+    return exp(value);
+}
+
+// Exponential function base 2
+JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_exp2(JNIEnv *env, jclass cls, jdouble value) {
+    return exp2(value);
+}
+
+// Exponential function base 10
+JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_exp10(JNIEnv *env, jclass cls, jdouble value) {
+    return pow(10, value);
+}
+
+// Power function
+JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_pow(JNIEnv *env, jclass cls, jdouble base, jdouble exponent) {
+    return pow(base, exponent);
+}
+// Rounding functions
+JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_round(JNIEnv *env, jclass cls, jdouble value) {
+    return round(value);
+}
+
+// Ceiling function
+JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_ceil(JNIEnv *env, jclass cls, jdouble value) {
+    return ceil(value);
+}
+
+// Floor function
+JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_floor(JNIEnv *env, jclass cls, jdouble value) {
+    return floor(value);
+}
+
+// Minimum function
+JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_min(JNIEnv *env, jclass cls, jdouble a, jdouble b) {
+    return fmin(a, b);
+}
+
+// Maximum function
+JNIEXPORT jdouble JNICALL Java_com_magayaga_microscript_NativeMath_max(JNIEnv *env, jclass cls, jdouble a, jdouble b) {
+    return fmax(a, b);
 }
